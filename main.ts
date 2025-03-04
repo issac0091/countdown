9a6548c6a057199b18fbba32c846287ba6d729fe
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-03-04 21:25:14 +0800

    更新 pxt.json, main.blocks, main.ts, pxt.json, _history

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-for (let index = 0; index <= 2; index++) {
-    basic.showNumber(3 - index)
-    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+OLED.init(128, 64)
+for (let index = 0; index <= 59; index++) {
+    basic.showNumber(60 - index)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.UntilDone)
 }
 basic.pause(500)
 music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
